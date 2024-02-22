7f73cadbe4a1fc2f130ab35e928c577ffe2709bd
Mark Durrant <mark.j.durrant@gmail.com>
2024-02-22 16:11:34 +0000

    AG-10421 - Style modal header search box

--- grid-packages/ag-grid-docs/documentation/src/components/search/Search.jsx
+++ grid-packages/ag-grid-docs/documentation/src/components/search/Search.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import SearchModal from './SearchModal';
+import { Icon } from '@components/Icon';
+
 import styles from '@design-system/modules/Search.module.scss';
+
 /**
  * grid-packages/ag-grid-docs/documentation
  * The website uses Algolia to power its search functionality. This component builds on components provided by Algolia
@@ -9,6 +12,10 @@ import styles from '@design-system/modules/Search.module.scss';
 const Search = ({ currentFramework }) => {
     const [isOpen, setOpen] = useState(false);
 
+    const isMacLike =
+        /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform) ||
+        /(Mac|iPhone|iPod|iPad)/i.test(navigator.userAgentData.platform);
+
     /**
      * When search is mounted, add global listeners to open/close the search
      */
@@ -16,6 +23,7 @@ const Search = ({ currentFramework }) => {
         const onKeyDownAnywhere = (e) => {
             const isMetaK = e.key === 'k' && (e.metaKey || e.ctrlKey);
             const isEsc = e.key === 'Escape';
+            
             if (isMetaK || isEsc) {
                 // use the callback so we don't need to update the func ref,
                 // and start removing/adding the callback, which would be messy
@@ -24,11 +32,17 @@ const Search = ({ currentFramework }) => {
         };
 
         document.addEventListener('keydown', onKeyDownAnywhere);
+
         return () => document.removeEventListener('keydown', onKeyDownAnywhere);
     }, []);
 
     return <>
-        <div className={styles.headerSearchBox} onClick={() => setOpen(true)}>Search documentation...<kbd>⌘ K</kbd>  </div>
+        <div className={styles.headerSearchBox} onClick={() => setOpen(true)}>
+            <Icon name="search" svgClasses={styles.searchIcon}/>
+            <span className={styles.placeholder}>Search...</span>
+            <span className={styles.kbdShortcut}>{ isMacLike ? `⌘ K` : `Ctrl K` }</span>
+        </div>
+        
         { isOpen && <SearchModal currentFramework={currentFramework} closeModal={() => setOpen(false)} /> }
     </>;
 }
