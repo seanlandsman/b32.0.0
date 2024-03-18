f64651444c1a9b03a11e3736d3823b7572886757
Bernie Sumption <bernie@berniecode.com>
2024-03-18 15:22:15 +0000

    WIP

--- grid-packages/ag-grid-theme-builder/src/ag-grid-community-themes/parts/tab-style/tab-styles.ts
+++ grid-packages/ag-grid-theme-builder/src/ag-grid-community-themes/parts/tab-style/tab-styles.ts
@@ -1,13 +1,31 @@
+import { ref, transparentForeground } from '../..';
 import { definePart } from '../../theme-utils';
 import { tabStyleRolodexCSS } from './GENERATED-tab-style-rolodex';
 import { tabStyleSimpleCSS } from './GENERATED-tab-style-simple';
 
+/**
+ * Tabs with configurable colours and borders, with an optional underline for the selected tab.
+ */
 export const tabStyleSimple = definePart({
   partId: 'tabStyle',
   variantId: 'simple',
+  additionalParams: {
+    tabBarBackgroundColor: transparentForeground(0.05),
+    tabBarBorder: true,
+    tabPadding: ref('gridSize'),
+    tabSelectedBackgroundColor: ref('backgroundColor'),
+    tabSelectedBorder: true,
+    tabSelectedUnderlineColor: ref('accentColor'),
+    tabSelectedUnderlineWidth: '0',
+    tabSelectedUnderlineTransitionDuration: '0',
+  },
   css: [tabStyleSimpleCSS],
 });
 
+/**
+ * Tabs where the selected tab appears raised and attached the the active
+ * content, like a rolodex or operating system tabs.
+ */
 export const tabStyleRolodex = definePart({
   partId: 'tabStyle',
   variantId: 'rolodex',
