fd0fceac8b2b2ba4beea1e623c3c8ff0af5af056
Bernie Sumption <bernie@berniecode.com>
2024-03-21 09:47:47 +0000

    WIP

--- grid-packages/ag-grid-theme-builder/tmp-types-play.ts
+++ grid-packages/ag-grid-theme-builder/tmp-types-play.ts
@@ -5,9 +5,15 @@ type PartValue<T extends string = never> = {
   defaults: Record<T, any>;
 };
 
-type PartFunction<T extends string = never> = () => PartValue<T>;
+// type PartFunction<T extends string = never> = (() => PartValue<T>) & {
+//   feature: string;
+// };
 
-type Part<T extends string = never> = PartValue<T> | PartFunction<T>;
+type PartFunction<TArgs, TParams extends string> = ((args?: TArgs) => PartValue<TParams>) & {
+  feature: string;
+};
+
+type Part<T extends string = never> = PartValue<T> | PartFunction<any, T>;
 
 type Parts<T extends string = never> = Part<T>[];
 
@@ -15,7 +21,7 @@ type InferParams<T> = T extends any[]
   ? InferParams<T[number]>
   : T extends PartValue<infer P>
     ? P
-    : T extends PartFunction<infer P>
+    : T extends PartFunction<any, infer P>
       ? P
       : never;
 
@@ -26,7 +32,7 @@ type DefinePartArgs<TExt extends Part<string>, TInc extends Parts<string>, TAdd 
   id: string;
   extend?: TExt;
   includeByDefault?: TInc;
-  addParams: Record<TAdd, any>;
+  addParams?: Record<TAdd, any>;
   //   overrideParams?: {[K in TOver]: K extends InferParams<TExt> | InferParams<TInc> ? any : never};
   overrideParams?: Partial<Record<InferParams<TExt> | InferParams<TInc>, any>>;
 };
@@ -38,45 +44,64 @@ const definePart = <
   TArgs = void,
 >(
   builder: (args?: TArgs) => DefinePartArgs<TExt, TInc, TAdd>,
-): ((args?: TArgs) => PartValue<InferParams<TExt> | InferParams<TInc> | TAdd>) => {
+): PartFunction<TArgs, InferParams<TExt> | InferParams<TInc> | TAdd> => {
   return undefined as any;
 };
 
-const a = definePart(() => ({
+const aFunc = definePart(() => ({
   id: 'a',
   addParams: { a: 1 },
   overrideParams: {},
 }));
 
-const blarty = definePart(() => ({
+const aValue = aFunc();
+
+const bFunc = definePart(() => ({
   id: 'blarty',
-  addParams: { blartyA: 'a', blartyB: 'b' },
+  addParams: { b: 2 },
   overrideParams: {},
 }));
 
-const b = definePart(() => ({
-  id: 'b',
-  extend: a,
-  addParams: { b: 8 },
-}));
+const bValue = bFunc();
 
-const c = definePart(() => ({
-  id: 'c',
-  addParams: { c: 5 },
-}));
-
-const ace = definePart(() => ({
-  id: 'ace',
-  includeByDefault: [a, c],
-  addParams: { e: 8 },
-}));
-
-const switcheroo = definePart((dongle?: boolean) => ({
-  id: 'switcheroo',
-  extend: ace,
-  includeByDefault: [blarty],
-  addParams: { __b: dongle ? 8 : 18 },
+const cFunc = definePart(() => ({
+  id: 'blarty',
+  addParams: { c: 2 },
   overrideParams: {},
 }));
 
-const switcherooValue = switcheroo();
+const cValue = cFunc();
+
+const extendsAIncludesBC = definePart(() => ({
+  id: 'extendsAIncludesBC',
+  extend: aValue,
+  includeByDefault: [bFunc, cValue],
+}));
+
+// Next up: put feature in definePart and set on function
+
+// Then add transferableAbortSignal, usiung type checking:
+
+type Equal<A, B> = 'notequal' extends
+  | (A extends B ? 'equal' : 'notequal')
+  | (B extends A ? 'equal' : 'notequal')
+  ? false
+  : true;
+
+// gives a type error if not equal
+const assertTypesEqual = <A, B>(a: Equal<A, B>) => {};
+
+assertTypesEqual<'foo', 'foo' | 'bar'>(true);
+
+const withArgs = definePart((arg?: boolean) => ({
+  id: 'extendsAIncludesBCWithArgs',
+  extend: aValue,
+  includeByDefault: [bFunc, cValue],
+}));
+
+withArgs(true);
+
+export const defineTheme = <P extends Part>(
+  partOrParts: P | P[],
+  parameters: Partial<Record<InferParams<P>, any>>,
+) => {};
